259b8bcd8ab86c48146878ffdd9e4eaa7e4cc983
SUJAL SRIVASTAVA <155046379+SUJAL-SRIVASTAVA@users.noreply.github.com>
2024-11-09 19:22:14 +0530

    BST.c

--- BST.c
+++ BST.c
@@ -1,114 +1,48 @@
-#include <iostream>  
-using namespace std;  
-struct Node {  
-    int data;  
-    Node *left;  
-    Node *right;  
-};  
-Node* create(int item)  
-{  
-    Node* node = new Node;  
-    node->data = item;  
-    node->left = node->right = NULL;  
-    return node;  
-}  
-/*Inorder traversal of the tree formed*/  
-void inorder(Node *root)  
-{  
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *right;
+    struct node *left;
+};
+
+struct node *createnew(int data){
+    struct node *temp=malloc(sizeof(struct node));
+    temp->data=data;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+
+struct node *insertionbst(struct node *root,int data){
+    if(root==NULL)
+        return createnew(data);
+    else if(data<root->data)
+        root->left=insertionbst(root->left,data);
+    else
+        root->right=insertionbst(root->right,data);
+    return root;
+}
+
+void inorder(struct node *root){
     if (root == NULL)  
-        return;  
-    inorder(root->left); //traverse left subtree  
-    cout<< root->data << "   "; //traverse root node  
-    inorder(root->right); //traverse right subtree  
-}  
-Node* findMinimum(Node* cur) /*To find the inorder successor*/  
-{  
-    while(cur->left != NULL) {  
-        cur = cur->left;  
-    }  
-    return cur;  
-}  
-Node* insertion(Node* root, int item) /*Insert a node*/  
-{  
-    if (root == NULL)  
-        return create(item); /*return new node if tree is empty*/  
-    if (item < root->data)  
-        root->left = insertion(root->left, item);  
-    else  
-        root->right = insertion(root->right, item);  
-    return root;  
-}  
-void search(Node* &cur, int item, Node* &parent)  
-{  
-    while (cur != NULL && cur->data != item)  
-    {  
-        parent = cur;  
-        if (item < cur->data)  
-            cur = cur->left;  
-        else  
-            cur = cur->right;  
-    }  
-}  
-void deletion(Node*& root, int item) /*function to delete a node*/  
-{  
-    Node* parent = NULL;  
-    Node* cur = root;  
-    search(cur, item, parent); /*find the node to be deleted*/  
-    if (cur == NULL)  
-        return;  
-    if (cur->left == NULL && cur->right == NULL) /*When node has no children*/  
-    {  
-        if (cur != root)  
-        {  
-            if (parent->left == cur)  
-                parent->left = NULL;  
-            else  
-                parent->right = NULL;  
-        }  
-        else  
-            root = NULL;  
-        free(cur);       
-    }  
-    else if (cur->left && cur->right)  
-    {  
-        Node* succ  = findMinimum(cur->right);  
-        int val = succ->data;  
-        deletion(root, succ->data);  
-        cur->data = val;  
-    }  
-    else  
-    {  
-        Node* child = (cur->left)? cur->left: cur->right;  
-        if (cur != root)  
-        {  
-            if (cur == parent->left)  
-                parent->left = child;  
-            else  
-                parent->right = child;  
-        }  
-        else  
-            root = child;  
-        free(cur);  
-    }  
-}  
-int main()  
-{  
-  Node* root = NULL;  
-  root = insertion(root, 45);  
-  root = insertion(root, 30);  
-  root = insertion(root, 50);  
-  root = insertion(root, 25);  
-  root = insertion(root, 35);  
-  root = insertion(root, 45);  
-  root = insertion(root, 60);  
-  root = insertion(root, 4);  
-  printf("The inorder traversal of the given binary tree is - \n");  
+        return; 
+    inorder(root->left);
+    printf("%d \t",root->data);
+    inorder(root->right);
+}
+
+int main(){
+  struct node* root = NULL;  
+  root = insertionbst(root, 45);  
+  root = insertionbst(root, 30);  
+  root = insertionbst(root, 50);  
+  root = insertionbst(root, 25);  
+  root = insertionbst(root, 35);  
+  root = insertionbst(root, 60);  
+  root = insertionbst(root, 4); 
+  printf("inorder traversal- \n"); 
   inorder(root);  
-  deletion(root, 25);  
-  printf("\nAfter deleting node 25, the inorder traversal of the given binary tree is - \n");  
-  inorder(root);   
-  insertion(root, 2);  
-  printf("\nAfter inserting node 2, the inorder traversal of the given binary tree is - \n");  
-  inorder(root);  
-  return 0;  
-}  
+    
+    return 0;
+}
