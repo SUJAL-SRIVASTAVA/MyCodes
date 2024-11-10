c7ff684a4494919faaa4a64cf60770dcdd67e939
SUJAL SRIVASTAVA <155046379+SUJAL-SRIVASTAVA@users.noreply.github.com>
2024-11-10 12:19:23 +0530

    Update BST.c

--- BST.c
+++ BST.c
@@ -32,6 +32,59 @@ void inorder(struct node *root){
     inorder(root->right);
 }
 
+
+struct node * search(struct node *root,int data){
+    struct node *t=root;
+    while(t!=NULL){
+        if(data==t->data){
+            return t;
+        }
+        else if(data<t->data){
+            t=t->left;
+        }
+        else if(data>t->data){
+            t=t->right;
+        }
+
+    }
+    return t;
+}
+
+    
+struct node* findMin(struct node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+struct node* deleteNode(struct node* root, int data) {
+    if (root == NULL) return root;
+
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    } else {
+        if (root->left == NULL) {
+            struct node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        struct node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+
+
 int main(){
   struct node* root = NULL;  
   root = insertionbst(root, 45);  
@@ -42,7 +95,37 @@ int main(){
   root = insertionbst(root, 60);  
   root = insertionbst(root, 4); 
   printf("inorder traversal- \n"); 
-  inorder(root);  
+  inorder(root); 
+   printf("\n");
+  struct node* temp=search(root,30);
+  if(temp!=NULL){
+      printf("Element found \n");
+  }
+  else{
+      printf("Not found\n");
+  }
+  temp=search(root,12);
+  if(temp!=NULL){
+      printf("Element found\n");
+  }
+  else{
+      printf("Not found\n");
+  }
+  temp=search(root,14);
+  if(temp!=NULL){
+      printf("Element found\n");
+  }
+  else{
+      printf("Not found\n");
+  }
+  deleteNode(root,60);
+  printf("inorder traversal- \n"); 
+  inorder(root);
+  printf("\n");
+  deleteNode(root,35);
+  printf("inorder traversal- \n"); 
+  inorder(root);
+  
     
     return 0;
 }
